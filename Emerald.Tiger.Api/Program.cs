88ec535fa5a00548d15ec29b3d5b0bb784eaf4e9
Chad Thomas <chad.thomas@neudesic.com>
2022-04-09 13:15:03 -0400

    adds app config to settings

--- Emerald.Tiger.Api/Program.cs
+++ Emerald.Tiger.Api/Program.cs
@@ -1,14 +1,40 @@
 using Emerald.Tiger.Data;
+using Emerald.Tiger.Api.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
 
+string authority = builder.Configuration["Auth0:Authority"] ??
+    throw new ArgumentNullException("Auth0:Authority");
+
+string audience = builder.Configuration["Auth0:Audience"] ??
+    throw new ArgumentNullException("Auth0:Audience");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = authority;
+        options.Audience = audience;
+    });
+
+builder.Services.AddAuthorization(options =>
+    {
+        options.AddPolicy("read:messages", policy =>
+            policy.Requirements.Add(new HasScopeRequirement("read:messages", authority)));
+    });
+
 builder.Services.AddDbContext<StoreContext>(options =>
-    options.UseSqlite("Data Source=../Registrar.sqlite", 
+    options.UseSqlite("Data Source=../Registrar.sqlite",
     b => b.MigrationsAssembly("Emerald.Tiger.Api"))
 );
 builder.Services.AddEndpointsApiExplorer();
@@ -26,6 +52,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
