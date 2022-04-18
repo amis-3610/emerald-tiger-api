9c1fa4946052abaf51905ccc67e175ea5a2ef4a3
Chad Thomas <chad.thomas@neudesic.com>
2022-04-18 14:32:23 -0400

    Database fix (#17)
    * adds notes to git ignore
    
    * seed data for database
    
    * adds Sqll conn string
    
    * moves from sqlite to Azure sql

--- Emerald.Tiger.Api/Program.cs
+++ Emerald.Tiger.Api/Program.cs
@@ -12,6 +12,9 @@ string authority = builder.Configuration["Auth0:Authority"] ??
 string audience = builder.Configuration["Auth0:Audience"] ??
     throw new ArgumentNullException("Auth0:Audience");
 
+string storeConnectionString = builder.Configuration.GetConnectionString("StoreConnection") ??
+    throw new ArgumentNullException("ConnectionString:StoreConnection");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -34,7 +37,7 @@ builder.Services.AddAuthorization(options =>
     });
 
 builder.Services.AddDbContext<StoreContext>(options =>
-    options.UseSqlite("Data Source=../Registrar.sqlite",
+    options.UseSqlServer(storeConnectionString,
     b => b.MigrationsAssembly("Emerald.Tiger.Api"))
 );
 builder.Services.AddCors(options =>
